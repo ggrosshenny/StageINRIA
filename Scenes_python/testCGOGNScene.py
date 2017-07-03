04e9c314eef164ef610c4c4b5f1b1713d07ba608
Guillaume <grosshenny.guillaume@gmail.com>
2017-07-03 13:27:19 +0200

    Ajout des scènes CGOGN en python et de la scène xml avec la régular grid.

--- Scenes_python/testCGOGNScene.py
+++ Scenes_python/testCGOGNScene.py
@@ -22,11 +22,16 @@ def createScene(node):
     # Topology settings
     poutreNode.createObject('VolumeTopologyContainer', src='@loader', name='Container', tetrahedra='@loader.tetrahedra')
     # Physic manager
-    poutreNode.createObject('CMTetrahedralCorotationalFEMForceField', name='CMCFEM', youngModulus='360', poissonRatio='0.3', method='large')
+    poutreNode.createObject('CMTetrahedralCorotationalFEMForceField', name='CMCFEM', youngModulus='1000', poissonRatio='0.2', method='large')
     # Set CGOGN topology
-    #poutreNode.createObject('TetrahedronSetTopologyModifier', name='Modifier')
-    #poutreNode.createObject('TetrahedronSetGeometryAlgorithms', template='Vec3d', name='GeomAlgo')
-
+    poutreNode.createObject('TetrahedronSetTopologyModifier', name='Modifier')
+    poutreNode.createObject('TetrahedronSetGeometryAlgorithms', template='Vec3d', name='GeomAlgo')
+    # BoxROI for fixed constraint (on the left)
+    poutreNode.createObject('BoxROI', template='Vec3d', box='1.995 -0.005 -0.005 2.035 0.065 0.0205', drawBoxes='1', position='@mecaObj.rest_position', name='FixedROI', computeTriangles='0', computeTetrahedra='0', computeEdges='0', tetrahedra='@Container.tetrahedra')
+    poutreNode.createObject('FixedConstraint', template='Vec3d', name='default6', indices='@FixedROI.indices')
+    # BoxROI for constant constraint (on the right)
+    poutreNode.createObject('BoxROI', template='Vec3d', box='1.995 -0.005 0.18 2.035 0.065 0.205', drawBoxes='1', position='@mecaObj.rest_position', name='constForceFieldROI', computeTriangles='0', computeTetrahedra='0', computeEdges='0', tetrahedra='@Container.tetrahedra')
+    poutreNode.createObject('ConstantForceField', points='constForceFieldROI.pointsInROI', force='0 -0.1 0', arrowSizeCoef='0.1')
     # Visual node
     VisualNode = poutreNode.createChild('Visu')
     VisualNode.createObject('OglModel', name='poutreVisual', fileMesh='mesh/poutre_surface.obj', color='red', dx='2.5')
